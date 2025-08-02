a4591537747f10713950fade560801656112f76c
Avishkar Dandge <vishkardandge@gmail.com>
2025-08-02 16:47:37 +0530

    fix imports and add admin user

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float, Boolean, Text, Date, Enum, Index
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from .database import Base
+from database import Base
 import enum
 
 class GenderEnum(enum.Enum):
